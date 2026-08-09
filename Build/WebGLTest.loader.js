e4874300ec7bc1e72e1314ef0afe3501a5532280
uchiuchi123 <52997418+uchiuchi123@users.noreply.github.com>
2026-08-09 19:20:58 +0900

    WebGL QA 1.0 RC6 2026-08-09T19:20:55+09:00

--- Build/WebGLTest.loader.js
+++ Build/WebGLTest.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68495869,
+    wasmFileSize: 68495806,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
